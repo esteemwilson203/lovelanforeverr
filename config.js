bb840425d355735463e0540f404ea6a0fb3c0da5
esteemwilson203 <semester4fpt@gmail.com>
2024-05-29 12:07:23 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,19 @@
+const CONFIG = {
+    titleWeb: "Gửi Lan người yêu của bé😊😊😊",
+    introTitle: 'Bé yêu Lan rất nhìu nì😊😊😘',
+    introDesc: `Trái đất vốn lạ thường
+    Mà sao cậu cứ đi nhầm đường
+    Lạc vào tim tớ 🤭🤭😌`,
+    btnIntro: '^^HiHi^^',
+    title: 'Bé yêu cậu nhiều lắm luôn á hihi 🥰',
+    desc: '🥰🥰🥰',
+    btnYes: 'Yêu lắm <33',
+    btnNo: 'Không nha :3',
+    question: 'Trên thế giới hơn 7 tỉ người mà tớ chỉ muốn yêu mình cậu hì hì<3',
+    btnReply: 'Gửi cậu<3',
+    reply: 'Yêu cậu rất nhiều hì hì <33333333',
+    mess: 'Bé biết mà 🥰. Bé yêu cậu nhiều nhiều 😘😘',
+    messDesc: 'Phải giữ gìn sức khỏe vào đó nhớ.',
+    btnAccept: 'Okiiiii lun <3',
+    messLink: 'https://www.facebook.com/profile.php?id=100025962289157' 
+}
